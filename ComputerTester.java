6d8cbcd454ae6483247dcff8cf6408001ca258cc
Surkuria Fisher <111896326+szf2@users.noreply.github.com>
2023-10-07 12:46:00 -0500

    Create ComputerTester.java

---
+++ ComputerTester.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+
+public class ComputerTester {
+
+	public static void main(String[] args) {
+		ComputerOrderCalculation cO = new ComputerOrderCalculation();
+		cO.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cO.setSize(500, 300);
+		cO.setVisible(true);
+		
+
+	}
+
+}
